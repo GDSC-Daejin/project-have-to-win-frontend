6b2431f28c2e963454870be4743dcd4fe5db9878
Hayeon-Jung <jhy00714@gmail.com>
2024-02-23 19:37:18 +0900

    모델선택 컴포넌트 제작

---
+++ src/app/component/SelectModel.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import styled from 'styled-components'
+
+const Container = styled.div`
+width: 266px;
+height: 190px;
+background-color: lightgray;
+
+display: flex;
+justify-content: center;
+flex-direction: column;
+align-items: center;
+justify-content: space-between;
+
+padding: 50px;
+` 
+
+const Button = styled.button`
+width: 140px;
+height: 30px;
+`
+
+const SelectModel = () => {
+
+
+  return (
+    <>
+      <Container>
+        <Button>Yolo 모델 선택</Button>
+        <Button>OCR 모델 선택</Button>
+      </Container>
+    </>
+  )
+}
+
+export default SelectModel
