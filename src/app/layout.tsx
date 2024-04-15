dde4ce23928fc1163141050ad7998e3b0359385c
Hayeon-Jung <jhy00714@gmail.com>
2024-04-15 20:07:58 +0900

    setting recoil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ReactQueryProviders from "./hooks/useReactQuery";
+import RecoilContextProvider from "./recoilContextProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ReactQueryProviders>{children}</ReactQueryProviders>
+        <ReactQueryProviders>
+          <RecoilContextProvider>{children}</RecoilContextProvider>
+        </ReactQueryProviders>
       </body>
     </html>
   );
