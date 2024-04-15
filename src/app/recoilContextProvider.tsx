5341230cca4fbedc9637e663e1738b4dcae7b5e3
Hayeon-Jung <jhy00714@gmail.com>
2024-04-15 20:08:15 +0900

    setting recoil

---
+++ src/app/recoilContextProvider.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { RecoilRoot } from "recoil";
+
+const RecoilContextProvider = ({ children }: { children: React.ReactNode }) => {
+  return <RecoilRoot>{children}</RecoilRoot>;
+};
+
+export default RecoilContextProvider;
