e678f1b97e837f512716c99666719bb2f6e9dab8
Hayeon-Jung <jhy00714@gmail.com>
2024-05-14 23:40:03 +0900

    공통 CSS 적용 및 Curtain 조건 추가 - 단계에 따른 Show 이벤트

--- src/app/component/FileDownload.tsx
+++ src/app/component/FileDownload.tsx
@@ -1,21 +1,29 @@
 "use client";
 
 import { useState } from "react";
-import { Button, Container, Curtain } from "./componentCSS";
+import { Button, Container, ContainerPadding, Curtain } from "./componentCSS";
 import { GoDownload } from "react-icons/go";
+import { workStateAtom } from "./atom";
+import { useRecoilState } from "recoil";
 
 const FileDownload = () => {
   const [hoverState, setHoverState] = useState("block");
 
+  const [workState, setWorkState] = useRecoilState(workStateAtom);
+
   return (
     <div>
-      <Curtain
-        display={hoverState}
-        onMouseEnter={() => setHoverState("none")}
-      />
       <Container onMouseLeave={() => setHoverState("block")}>
-        <GoDownload color={"skyblue"} size={100} />
-        <Button>모델 다운로드</Button>
+        <ContainerPadding>
+          <GoDownload color={"skyblue"} size={100} />
+          <Button>모델 다운로드</Button>
+        </ContainerPadding>
+        {workState !== 3 && (
+          <Curtain
+            display={hoverState}
+            onMouseEnter={() => setHoverState("none")}
+          />
+        )}
       </Container>
     </div>
   );
