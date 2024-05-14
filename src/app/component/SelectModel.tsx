9f62575e8117f67a53a603f1a13d46cd4ec73904
Hayeon-Jung <jhy00714@gmail.com>
2024-05-14 23:40:41 +0900

    공통 CSS 적용 및 Curtain 조건 추가 - 단계에 따른 Show 이벤트

--- src/app/component/SelectModel.tsx
+++ src/app/component/SelectModel.tsx
@@ -2,41 +2,44 @@
 
 import { useRecoilState, useRecoilValue } from "recoil";
 import styled from "styled-components";
-import { modelTypeAtom, resumeAtom } from "./atom";
-import { Button, Container, Curtain } from "./componentCSS";
+import { modelTypeAtom, resumeAtom, workStateAtom } from "./atom";
+import { Button, Container, ContainerPadding, Curtain } from "./componentCSS";
 import { useState } from "react";
 
 const SelectModel = () => {
   const [hoverState, setHoverState] = useState("block");
   const [modelType, setModelType] = useRecoilState(modelTypeAtom);
+  const [workState, setWorkState] = useRecoilState(workStateAtom);
+
   const resumeType = useRecoilValue(resumeAtom);
 
   return (
     <div>
-      <Curtain
-        display={hoverState}
-        onMouseEnter={() => setHoverState("none")}
-      />
-      <Container
-        onMouseLeave={() => setHoverState("block")}
-        padding="200px 10px"
-      >
-        <Button
-          onClick={() => {
-            setModelType("YOLO");
-          }}
-          disabled={resumeType}
-        >
-          Yolo 모델학습
-        </Button>
-        <Button
-          onClick={() => {
-            setModelType("OCR");
-          }}
-          disabled={resumeType}
-        >
-          OCR 모델학습
-        </Button>
+      <Container onMouseLeave={() => setHoverState("block")}>
+        <ContainerPadding padding="200px 10px">
+          <Button
+            onClick={() => {
+              setModelType("YOLO");
+            }}
+            disabled={resumeType}
+          >
+            Yolo 모델학습
+          </Button>
+          <Button
+            onClick={() => {
+              setModelType("OCR");
+            }}
+            disabled={resumeType}
+          >
+            OCR 모델학습
+          </Button>
+        </ContainerPadding>
+        {workState !== 1 && (
+          <Curtain
+            display={hoverState}
+            onMouseEnter={() => setHoverState("none")}
+          />
+        )}
       </Container>
     </div>
   );
