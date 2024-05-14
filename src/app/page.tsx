cf800fd9d975dd9d1da120b59b1ead7f2e113dee
Hayeon-Jung <jhy00714@gmail.com>
2024-05-14 23:39:05 +0900

    사용하지 않는 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import LearningOption from "./component/LearningOption";
 import SelectModel from "./component/SelectModel";
