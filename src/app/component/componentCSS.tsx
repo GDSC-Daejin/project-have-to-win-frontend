de6028abedc5d558eb8caa3fdcd0893af01a5304
Hayeon-Jung <jhy000714@gmail.com>
2024-05-14 16:29:56 +0900

    공통 CSS 컨테이너 패딩값 수정

--- src/app/component/componentCSS.tsx
+++ src/app/component/componentCSS.tsx
@@ -11,7 +11,7 @@ export const Container = styled.div<{ padding?: string }>`
   border-radius: 8px;
   width: 280px;
   height: 500px;
-  padding: ${(props) => props.padding || "120px 10px"};
+  padding: ${(props) => props.padding || "140px 10px 190px 10px"};
   background-color: white;
 `;
 
@@ -51,6 +51,28 @@ export const Input = styled.input`
   cursor: pointer;
 `;
 
+export const InputButton = styled.input`
+  border-style: solid;
+  border-width: 1px;
+  border-color: skyblue;
+  border-radius: 3px;
+  font-weight: 700;
+  color: skyblue;
+  width: 200px;
+  height: 35px;
+  padding: 5px 10px;
+  margin: 5px 0;
+  background-color: white;
+  cursor: pointer;
+  &:hover {
+    color: white;
+    background-color: skyblue;
+    border: none;
+  }
+  &:disabled {
+  }
+`;
+
 export const Select = styled.select`
   border-style: solid;
   border-width: 1px;
@@ -82,5 +104,7 @@ export const Button = styled.button`
     color: white;
     background-color: skyblue;
     border: none;
+  }
+  &:disabled {
   }
 `;
