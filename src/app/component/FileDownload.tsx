f434d9b0e8796ab5cfbf706cc0eb02e3d287a610
Hayeon-Jung <jhy00714@gmail.com>
2024-05-22 00:51:30 +0900

    files api 붙임... data에 경로가 배열 형태로 들어가있음

--- src/app/component/FileDownload.tsx
+++ src/app/component/FileDownload.tsx
@@ -5,11 +5,21 @@ import { Button, Container, ContainerPadding, Curtain } from "./componentCSS";
 import { GoDownload } from "react-icons/go";
 import { workStateAtom } from "./atom";
 import { useRecoilState } from "recoil";
+import { useQuery } from "@tanstack/react-query";
 
 const FileDownload = () => {
   const [hoverState, setHoverState] = useState("block");
 
   const [workState, setWorkState] = useRecoilState(workStateAtom);
+
+  const { data } = useQuery({
+    queryKey: ["files"],
+    queryFn: async () => {
+      return (await fetch(`http://122.47.121.165:8070/files`)).json();
+    },
+  });
+
+  console.log(data.files[0]);
 
   return (
     <div>
